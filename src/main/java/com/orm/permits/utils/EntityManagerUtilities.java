fe7e0b621f7ecf586139187e8796077f116e6ac9
evanfa <fabio.a.rodriguez@gmail.com>
2022-10-15 10:43:12 -0500

    Working Connection - Hibernate JPA

--- src/main/java/com/orm/permits/utils/EntityManagerUtilities.java
+++ src/main/java/com/orm/permits/utils/EntityManagerUtilities.java
@@ -13,8 +13,6 @@ public class EntityManagerUtilities {
 
     public static void main(String[] args) {
         //Security.setProperty("jdk.tls.disabledAlgorithms","SSLv3, RC4, DES, MD5withRSA, DH keySize < 1024,EC keySize < 224, 3DES_EDE_CBC, anon, NULL");
-
-
         EntityManager manager = EntityManagerUtilities.getEntityManager();
         System.out.println("Entity Manager Class ==> "+manager.getClass().getCanonicalName());
     }
