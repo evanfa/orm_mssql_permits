eaf18c05a89a339d63c5e9538b487db8f3e348f1
evanfa <fabio.a.rodriguez@gmail.com>
2022-11-10 18:11:34 -0600

    Update pom.xml - Working and Tested

--- src/main/java/com/orm/permits/app/App.java
+++ src/main/java/com/orm/permits/app/App.java
@@ -9,14 +9,20 @@ public class App {
     public static void main(String[] args) {
         AgencyDAO dao = new AgencyDAO();
         List<Agency> getAgencies = dao.getAll();
-        System.out.println("Listing Agency Details...");
-        System.out.println("***"+getAgencies);
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Listing Agency Details... "+getAgencies.size());
+        //System.out.println("***"+getAgencies);
 
-        Agency nAgency = new Agency("Comisión Federal de Electricidad","Rayito de Luz",12,34,"L-V","8-2","Sin anotaciones","P-123");
+            getAgencies.forEach((n)-> System.out.println(n));
+
+        System.out.println("---------------------------------------------------------");
+
+        /*Agency nAgency = new Agency("Comisión Federal de Electricidad","Relampago Veloz",12,34,"L-V","8-2","Sin anotaciones","P-123");
         System.out.println("Insert: "+nAgency);
 
         dao.saveRecord(nAgency);
         System.out.println("Done: "+nAgency);
+        */
 
     }
 }
